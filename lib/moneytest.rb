483e7dfbb83af7082d1acc92683dfe266354a4e9
t00l <moisesdonoso@gmail.com>
2017-12-04 17:22:37 -0300

    demo

--- lib/moneytest.rb
+++ lib/moneytest.rb
@@ -2,8 +2,27 @@ require "moneytest/version"
 
 module Moneytest
   class Money
-    def self.hi
-      puts "hola"
+
+    def initialize (amount, type)
+      @amount = amount
+      @type = type
     end
+
+    def amount
+      @amount
+    end
+    
+    def currency
+      @type
+    end
+
+    def inspect
+      "#{amount} #{@type}"
+    end
+
+    # class convert_to(data)
+    #   @amount
+    # end
+
   end
 end
