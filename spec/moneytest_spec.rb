ad18dec50786fdf8c4aed6f4659148b2098f11c6
t00l <moisesdonoso@gmail.com>
2017-12-12 21:21:24 -0300

    test convert

--- spec/moneytest_spec.rb
+++ spec/moneytest_spec.rb
@@ -5,3 +5,11 @@ describe 'test Money Class' do
     money = Money.new(50, 'USD')
   end
 end
+
+describe 'test Convert Method' do 
+  it 'get convert' do
+    fifty_eur = Money.new(50, 'EUR')
+    conv_usd = fifty_eur.convert('USD')
+    expect(conv_usd).to have_attributes(amount: 55.50000000000001, currency: 'USD')     
+  end
+end
